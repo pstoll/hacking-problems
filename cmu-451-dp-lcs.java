7ef5134a80fac7036a95af89f3541dff6c14cae5
Perry A. Stoll <perry@pstoll.com>
2014-12-17 19:17:27 -0500

    added backtracking to find the actual string, not just length

--- cmu-451-dp-lcs.java
+++ cmu-451-dp-lcs.java
@@ -13,12 +13,11 @@ class CMU_451_DP_LCS {
 	String s1;
 	String s2;
     }
-
+    
     static public class DPResult {
 	int[][] state;
-
     }
-
+    
     public static void dump_state(int[][] s) {
 	int yy = s.length;
 	int xx = s[0].length;
@@ -31,6 +30,42 @@ class CMU_451_DP_LCS {
 	}
     }
     
+    public static int clamp(int v1, int vmin, int vmax) {
+	if (v1 < vmin) return vmin;
+	if (v1 > vmax) return vmax;
+	return v1;
+    }
+
+    public static String backtrack_state(String s1, String s2, DPResult r) {
+	int max_i = s1.length() - 1;
+	int max_j = s2.length() - 1;
+	int i = max_i;
+	int j = max_j;
+	StringBuffer buf = new StringBuffer();
+
+	while (i > 0 || j > 0) {
+	    System.out.println("(i,j) = " + i + "," + j);
+	    int next_i = clamp(i - 1, 0, max_i);
+	    int next_j = clamp(j - 1, 0, max_j);
+	    System.out.println( "(next_i,nextj) = " + next_i + "," + next_j );
+	    if (r.state[i][j] == r.state[next_i][j] && i > 0) {
+		System.out.println("going up");
+		i = next_i;
+	    } else if (r.state[i][j] == r.state[i][next_j]  && j > 0) { 
+		System.out.println("going left");
+		j = next_j;
+	    } else {
+		System.out.println("appending char at ("+i+","+j+") " + s1.charAt(i));
+		buf.append( s1.charAt(i) );
+		i--;
+		j--;
+	    }
+	    System.out.println("(i,j) = " + i + "," + j + "\n");
+	}
+
+	return buf.reverse().toString();
+    }
+
     public static DPResult compute_lcs(DPInput in) {
 	return compute_lcs(in.s1, in.s2);
     }
@@ -41,7 +76,7 @@ class CMU_451_DP_LCS {
 
 	int[][] state = new int[yy][xx];
 	for ( int ii = 0; ii < yy; ii++) {
-	    for (int jj=0; jj<xx; jj++) {
+	    for (int jj=0; jj < xx; jj++) {
 		if (s1.charAt(ii) != s2.charAt(jj)) {
 		    // handle boundary conditions.
 		    int v1 = (ii == 0) ? 0 : state[ii-1][jj];
@@ -80,6 +115,8 @@ class CMU_451_DP_LCS {
 	    int x = res.state[0].length - 1;
 	    int len = res.state[y][x];
 	    System.out.println("length of longest common substring: " + len);
+	    String m = backtrack_state(in.s1, in.s2, res);
+	    System.out.println("match: " + m);
 	}
     }
 
